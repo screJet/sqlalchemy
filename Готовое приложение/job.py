80d0cabc754e16500b234fed0954b57da984a88c
screJet <64700446+screJet@users.noreply.github.com>
2020-05-11 23:48:48 +0600

    Add files via upload

---
+++ Готовое приложение/job.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+from data import db_session
+from data.jobs import Jobs
+from data.login_form import LoginForm
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/mars_explorer.sqlite")
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        session = db_session.create_session()
+        return session.query(User).get(user_id)
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            session = db_session.create_session()
+            user = session.query(User).filter(User.email == form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return redirect("/")
+            return render_template('login.html', message="Wrong login or password", form=form)
+        return render_template('login.html', title='Authorization', form=form)
+
+    @app.route("/")
+    def index():
+        session = db_session.create_session()
+        jobs = session.query(Jobs).all()
+        users = session.query(User).all()
+        names = {name.id: (name.surname, name.name) for name in users}
+        return render_template("index.html", jobs=jobs, names=names, title='Work log')
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return redirect("/")
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
